e601afe4b331838cbc2f3bd510f7f95ad2c448ce
Álvaro Alencar <alvaroromario1@hotmail.com>
2023-01-15 15:45:56 -0300

    feat: add handleErrorMiddleware and productRoutes to app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,14 @@ import "express-async-errors";
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { productRoutes } from "./routes/product/products.routes";
+import { handleErrorMiddleware } from "./middlewares/handleError.middleware";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/products", productRoutes);
+app.use(handleErrorMiddleware);
 
 export { app };
