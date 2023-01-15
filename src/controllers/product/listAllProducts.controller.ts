023fdf8feeba450a018f6165e8d17ad10cbba389
Álvaro Alencar <alvaroromario1@hotmail.com>
2023-01-15 16:33:17 -0300

    feat: add option to include deleted products while listing

--- src/controllers/product/listAllProducts.controller.ts
+++ src/controllers/product/listAllProducts.controller.ts
@@ -2,7 +2,9 @@ import { Request, Response } from "express";
 import { listAllProductsService } from "../../services/product/listAllProducts.service";
 
 export const listAllProductsController = async (req: Request, res: Response) => {
-  const productsList = await listAllProductsService();
+  const includeDeleted = req.query.includeDeleted === "true";
+
+  const productsList = await listAllProductsService(includeDeleted);
 
   return res.status(200).json(productsList);
 };
